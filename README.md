fd4508eb81dd41a2d2c8edf9e03361f532ba61c2
Sara Cope <s010sdc@gmail.com>
2021-10-31 20:36:27 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# YNAB Transactions Report
+
+## Demo
+
+TBD
+
+## Usage
+
+* Cloning the repo
+* In your terminal, run `npm install`
+* then `npm start`
+* Authorize the app then select a budget
+
+## Scripts
+
+### `npm start`
+Runs the development server (defaults to `localhost:8080`) and watches for changes.
+
+### `npm run build`
+Builds the production assets for deployment. This will build to `dist/build.js`
+which the `index.html` will load.
+
+
---
+++ src/components/Categories.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h5>Categories</h5>
+    <table class="table">
+    <thead>
+      <tr>
+        <th>Category</th>
+        <th>Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="category in categories" v-bind:key="category.id">
+        <td>{{category.category_name}}</td>
+        <td>{{convertMilliUnitsToCurrencyAmount(category.amount).toFixed(2)}}</td>
+      </tr>
+    </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+// Import utils from YNAB
+import {utils} from 'ynab';
+
+export default {
+  props: ['categories'],
+  methods: {
+    // Now we can make this method available to our template
+    // So we can format this milliunits in the correct currency format
+    convertMilliUnitsToCurrencyAmount: utils.convertMilliUnitsToCurrencyAmount
+  }
+}
+</script>
